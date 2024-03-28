6ff7a75c9531a422d83ccdd1ca487216c5c91d56
calvinmoylan <calvinmoylan1@gmail.com>
2024-03-28 23:46:04 +0800

    fixed info button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,7 +18,7 @@ const Home = () => {
         {/* website graphic */}
         <KengButton
           type="info"
-          className="absolute left-1/2 top-[53.5%]  z-50 w-[20%] -translate-x-1/2"
+          className="absolute left-1/2 top-[50.5%]  z-50 w-[20%] -translate-x-1/2"
           onClick={handleBuyClicked}
         />
 
